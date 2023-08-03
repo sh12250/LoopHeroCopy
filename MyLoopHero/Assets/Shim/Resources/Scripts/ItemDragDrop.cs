43fbe7089a6cd20c3ba22bb6a925328868ec6011
JB <sjbh3mc@gmail.com>
2023-08-03 20:53:25 +0900

    test. added item UI, colliders and testing raycast

---
+++ MyLoopHero/Assets/Shim/Resources/Scripts/ItemDragDrop.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class ItemDragDrop: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    //Vector3 mousePoint
+    //Ray ray;
+
+
+    private void Awake()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData) 
+    {
+    
+    }
+
+    public void OnPointerDown(PointerEventData eventData) 
+    {
+    
+    }
+
+    public void OnPointerUp(PointerEventData eventData) 
+    {
+    
+    }
+
+    public void OnDrag(PointerEventData eventData) 
+    {
+    
+    }
+}
