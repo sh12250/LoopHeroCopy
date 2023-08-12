ce336912268f72cb11bac57d344aa466ababab65
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-08-12 10:37:17 +0900

    Feat. MonsterManager instance 추가

--- MyLoopHero/Assets/Jung/Scripts/Managers/MonsterManager.cs
+++ MyLoopHero/Assets/Jung/Scripts/Managers/MonsterManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class MonsterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static MonsterManager instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null || instance == default)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("MonsterManager가 너무 많습니다");
+        }
     }
 }
